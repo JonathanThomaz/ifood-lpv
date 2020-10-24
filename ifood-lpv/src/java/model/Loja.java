4bc6cbf4dd4a6df6d70217f21dc0af4d824d7f44
Artur Abrão <arturmabrao@gmail.com>
2020-10-24 16:47:17 -0300

    Model

---
+++ ifood-lpv/src/java/model/Loja.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import model.conta.Conta;
+
+/**
+ *
+ * @author Artur
+ */
+public class Loja {
+    
+    private long id;
+    private String nome;
+    private String cnpj;
+    private String descricao;
+    private String imagem;
+
+    private Endereco endereco;
+    private Conta conta;
+    private Contato contato;
+
+    
+}
