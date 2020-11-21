04eadfa8c21552a1dc9584527aa75914d45dc5a8
Artur Abrão <arturmabrao@gmail.com>
2020-11-21 11:31:58 -0300

    F

--- src/java/model/MainFactory.java
+++ src/java/model/MainFactory.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
 
 import model.pedido.Pedido;
@@ -11,12 +6,8 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.usuario.Conta;
 
-/**
- *
- * @author mathe
- */
+
 public class MainFactory {
 
     public static Object getObject(String state) {
